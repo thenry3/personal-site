7c68deb3f5e59b61140d0f0f8ccdcee3991e2d13
Henry <henrymtrinh14@gmail.com>
2020-04-21 02:27:33 -0700

    cleanup

---
+++ src/pages/landing.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const PageWrapper = styled("div")`
+  display: flex;
+`;
+
+export default class LandingPage extends React.Component {
+  render() {
+    return (
+      <>
+        <PageWrapper></PageWrapper>
+      </>
+    );
+  }
+}
